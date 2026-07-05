8f57041cfdc8782c2cd4cf09ab8a164967405b80
repo-tech <hgupta414243@gmail.com>
2026-07-05 19:16:29 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+
+  experimental: {
+    optimizePackageImports: [
+      "@google/genai",
+      "mongodb",
+    ],
+  },
 };
 
 export default nextConfig;
